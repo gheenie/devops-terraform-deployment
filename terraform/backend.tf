979d92bd00b7d81d5ff939631a440ac0a7d2bfea
infoomics <sbx_94@hotmail.com>
2023-03-10 13:45:45 +0000

    change to unique bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "nc-jm-de-cicd-demo-state"
+        bucket = "nc-jm-de-cicd-demo-state-001"
         key = "s3-file-reader/terraform.tfstate"
         region = "us-east-1"
     }
