04c3a5610531ce36d4a8042ba2f0c134dc9ac0a0
infoomics <sbx_94@hotmail.com>
2023-03-13 18:10:10 +0000

    add untested scheduler and alarm

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,8 +10,26 @@ resource "aws_lambda_function" "s3_file_reader" {
 
 resource "aws_lambda_permission" "allow_s3" {
   action = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_file_reader.function_name
   principal = "s3.amazonaws.com"
+  function_name = aws_lambda_function.s3_file_reader.function_name
   source_arn = aws_s3_bucket.data_bucket.arn
+  source_account = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_cloudwatch_event_rule" "scheduler" {
+    name_prefix = "mistaker-scheduler-"
+    schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "scheduler_target" {
+  rule      = aws_cloudwatch_event_rule.scheduler.name
+  arn       = aws_lambda_function.s3_file_reader.arn
+}
+
+resource "aws_lambda_permission" "allow_scheduler" {
+  action = "lambda:InvokeFunction"
+  principal = "events.amazonaws.com"
+  function_name = aws_lambda_function.s3_file_reader.function_name
+  source_arn = aws_cloudwatch_event_rule.scheduler.arn
   source_account = data.aws_caller_identity.current.account_id
 }
